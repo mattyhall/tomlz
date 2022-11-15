02efaf3513ceb42262d952aee5c56d8c4aaa321d
Matt Hall <matthew@quickbeam.me.uk>
2022-11-15 19:15:15 +0000

    Move toml2json functionality out of main
    Main should be the entrypoint of the library

---
+++ src/toml2json.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const testing = std.testing;
+const lex = @import("lexer.zig");
+const parser = @import("parser.zig");
+
+pub fn main() !void {
+    if (std.os.argv.len != 2) {
+        std.debug.print("Please pass a TOML file as the second argument", .{});
+    }
+
+    var gpa = std.heap.page_allocator;
+
+    var f = try std.fs.openFileAbsoluteZ(std.os.argv[1], .{});
+    defer f.close();
+
+    var contents = try f.reader().readAllAlloc(gpa, 5 * 1024 * 1024);
+    defer gpa.free(contents);
+
+    var lexer = parser.Lexer{ .real = try lex.Lexer.init(gpa, contents) };
+    var p = try parser.Parser.init(gpa, lexer);
+    defer p.deinit();
+
+    var table = p.parse() catch |err| {
+        std.debug.print("error parsing {s}: {}\n", .{ std.os.argv[1], err });
+        std.debug.print("{?}\n", .{p.diag});
+        return err;
+    };
+    defer table.deinit(gpa);
+
+    const e2e = @import("end_to_end.zig");
+    var json = try e2e.tableToJson(gpa, &table);
+
+    var al = std.ArrayList(u8).init(gpa);
+    defer al.deinit();
+
+    try json.jsonStringify(.{ .whitespace = .{} }, al.writer());
+    std.debug.print("{s}", .{al.items});
+}
