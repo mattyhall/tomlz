741857b5c5b8aefc6fc1be9a13a380df269762e9
Matt Hall <matthew@quickbeam.me.uk>
2022-11-15 21:39:58 +0000

    Update example formatting

--- examples/simple/src/main.zig
+++ examples/simple/src/main.zig
@@ -8,14 +8,14 @@ pub fn main() !void {
         \\age = 46
         \\world_titles = [2001, 2004, 2008, 2012, 2013, 2020, 2022]
     ;
-    
+
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    
+
     var table = try tomlz.parse(gpa.allocator(), toml);
     defer table.deinit(gpa.allocator());
-    
+
     const goat = table.getTable("goat").?;
-    std.debug.print("GOAT: {s} (age {})\n", .{goat.getString("name").?, goat.getInteger("age").?});
+    std.debug.print("GOAT: {s} (age {})\n", .{ goat.getString("name").?, goat.getInteger("age").? });
     std.debug.print("Number of world titles: {}\n", .{goat.getArray("world_titles").?.items().len});
 }
