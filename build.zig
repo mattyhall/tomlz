a33f413807632dd3ba3fed53b6d326f8ed0f6cee
Matt Hall <matthew@quickbeam.me.uk>
2022-11-20 21:38:18 +0000

    Add ability to fuzz
    I tried follow a blog post [1] on how to fuzz with Zig. Unfortunately
    all but the slow version don't work for me. Luckily that is enough to
    find a few crashes.
    
    To run I've added a new binary that parses stdin. That can then be run
    from the AFL++ docker image [2] like so:
    
    ```
    afl-fuzz -i inputs -o outputs -x toml.dict -O ./zig-out/bin/fuzz
    ```
    
    [1] https://www.ryanliptak.com/blog/fuzzing-zig-code/
    [2] https://github.com/AFLplusplus/AFLplusplus/blob/stable/docs/INSTALL.md#linux-on-x86

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
@@ -14,4 +14,11 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    const fuzz_exe = b.addExecutable("fuzz", "src/fuzz.zig");
+    fuzz_exe.setBuildMode(mode);
+    fuzz_exe.linkLibC();
+    fuzz_exe.install();
+    const fuzz_compile_run = b.step("fuzz", "Build executable for fuzz testing afl-fuzz");
+    fuzz_compile_run.dependOn(&fuzz_exe.install_step.?.step);
 }
---
+++ src/fuzz.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const testing = std.testing;
+const lex = @import("lexer.zig");
+const parser = @import("parser.zig");
+
+export fn cmain() void {
+    main() catch unreachable;
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(!gpa.deinit());
+    var allocator = gpa.allocator();
+
+    const stdin = std.io.getStdIn();
+    const data = try stdin.readToEndAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(data);
+
+    var lexer = parser.Lexer{ .real = try lex.Lexer.init(allocator, data) };
+    var p = try parser.Parser.init(allocator, lexer);
+    defer p.deinit();
+
+    var table = p.parse() catch |err| {
+        std.debug.print("error parsing {}\n", .{ err });
+        std.debug.print("{?}\n", .{p.diag});
+        return;
+    };
+    defer table.deinit(allocator);
+}
