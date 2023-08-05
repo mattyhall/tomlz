8ee9b9712767d654872a87490460c5cc4bc12816
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2023-08-05 22:45:22 +0200

    purge gyro and zigmod, update README

--- examples/simple/build.zig
+++ examples/simple/build.zig
@@ -2,29 +2,29 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable(.{
+        .name = "simple",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    
+    // If we have the project in our repository then we can just add it as a module
+    const tomlz = b.addModule("tomlz", .{
+        .source_file = .{ .path = "../../src/main.zig"},
+    });
 
-    const exe = b.addExecutable("simple", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    // If we have the project in our repository then we can just add it as a package
-    exe.addPackagePath("tomlz", "../../src/main.zig");
-    exe.install();
+    exe.addModule("tomlz", tomlz);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+    
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
