dacb16dc5a8a1e620475bb5c06a81cc781916e6b
Milan Aleksić <milanaleksic@gmail.com>
2024-06-08 07:25:52 +0200

    revert changes to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,25 +2,48 @@
   description = "A TOML library for Zig";
 
   inputs = {
-    # 0.12.0 is on revision 07c88f35d25ba5d0fff5074900582ce89c0d6ad0
-    # look at: https://lazamar.co.uk/nix-versions/?package=zig
-    nixpkgs-zig-pinned.url = "github:NixOS/nixpkgs/07c88f35d25ba5d0fff5074900582ce89c0d6ad0";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zls = {
+      url = "github:erikarvstedt/zls/fix-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs-zig-pinned, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-        devShell =
-          with nixpkgs-zig-pinned.legacyPackages.${system};
-          mkShell {
-            buildInputs = [
-              nixpkgs-zig-pinned.legacyPackages.${system}.zig
-              nixpkgs-zig-pinned.legacyPackages.${system}.zls
-              nixpkgs-zig-pinned.legacyPackages.${system}.lldb
-            ];
-            shellHook = ''
-              '';
-          };
-      }
-  );
+  outputs = {self, nixpkgs, flake-utils, flake-compat, zig, zls}:
+    let
+      overlays = [
+        (final: prev: {
+          zigpkgs = zig.packages.${prev.system};
+        })
+        (final: prev: {
+          zlspkgs = zls.packages.${prev.system};
+        })
+      ];
+      systems = builtins.attrNames zig.packages;
+    in
+      flake-utils.lib.eachSystem systems (system:
+        let
+          pkgs = import nixpkgs { inherit overlays system; };
+        in
+          rec {
+            devShell = pkgs.mkShell {
+              buildInputs = (with pkgs; [
+                zigpkgs.master-2023-10-30
+                zlspkgs.default
+                bashInteractive
+                gdb
+                lldb
+              ]);
+            };
+          }
+      );
 }
